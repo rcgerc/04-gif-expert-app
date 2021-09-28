07d54049a7e3926918d442f4ecc71a937daed7a0
Gonzalo Retamal <rc_gonzalo@live.com>
2021-09-28 17:12:16 -0300

    GifExpertApp.js: Componente creado y probado

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () => {
+        setCategories([...categories, 'pokemon']);
+    }
+
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+
+            <ol>
+                { 
+                
+                categories.map( category => {
+                    return <li key={ category }> {category} </li>;
+                })
+
+                }
+            </ol>
+
+            <button onClick={ handleAdd } > Agregar </button>
+
         </>
     )
 }
